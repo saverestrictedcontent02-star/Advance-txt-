2def292ec056357e308838f9588bb4260fbd9397
saverestrictedcontent02-star <saverestrictedcontent02@gmail.com>
2026-01-02 17:27:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -572,7 +572,7 @@ async def upload(bot: Client, m: Message):
     #if thumb.startswith("http://") or thumb.startswith("https://"):
         #getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
         #thumb = "thumb.jpg"
-        thumb = "no"
+        thumb ="no"
     failed_count = 0
     if len(links) == 1:
         count = 1
