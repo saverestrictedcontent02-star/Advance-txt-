a7d7962ac46163ae353d17f2b43b88df4a2c2c63
saverestrictedcontent02-star <saverestrictedcontent02@gmail.com>
2026-01-03 13:46:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 import os
+import threading
+import main  # 👈 main.py import
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "OK"
+def home():
+    return "Bot is running"
 
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    # Flask server thread
+    t = threading.Thread(target=run_flask)
+    t.start()
+
+    # Telegram bot start
+    main.main()
